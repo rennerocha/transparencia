cfec7dbebcd6719de5e8f848175a1cee89890f2f
Renne Rocha <renne@rennerocha.com>
2019-02-28 22:39:51 -0300

    Filtered invalid twitter accounts

--- transparencia/items.py
+++ transparencia/items.py
@@ -1,3 +1,5 @@
+import re
+
 import scrapy
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import Compose, TakeFirst
@@ -12,6 +14,15 @@ def find_most_likely_url(values):
 def remove_dupes(values):
     if values:
         return list(set(values))
+
+
+def remove_invalid_twitter_urls(values):
+    urls = remove_dupes([url.lower() for url in values])
+    for url in urls:
+        user_m = re.match("twitter\.com\/(?P<username>@?[\w]*)$", url)
+        if user_m:
+            return 'https://twitter.com/{}'.format(user_m.group("username"))
+    return values
 
 
 class CityItem(scrapy.Item):
@@ -30,4 +41,4 @@ class CityItemLoader(ItemLoader):
 
     transparency_url_out = Compose(remove_dupes, find_most_likely_url)
     all_transparency_url_out = Compose(remove_dupes)
-    twitter_out = Compose(remove_dupes)
+    twitter_out = Compose(remove_dupes, remove_invalid_twitter_urls)
